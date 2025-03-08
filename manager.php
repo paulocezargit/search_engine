2d5f717caf52eba59c3979ba06366ac71984de73
Paulo Cezar <32718867+paulocezarphp@users.noreply.github.com>
2025-03-08 09:13:22 -0300

    Creating the records page
    This time, we started creating a page to save records, remembering that records in search systems can be saved dynamically or manually.

--- manager.php
+++ manager.php
@@ -1,5 +1,28 @@
 <?php include("header.php"); ?>
 
-
+<section class="s-manager">
+	<div class="s-manager-box">
+		<h1 class="s-manager-title">Register Site</h1>
+		<div class="s-manager-group">
+			<label class="s-manager-label">Name</label>
+			<input type="text" name="" class="s-manager-input" placeholder="Website name">
+		</div>
+		<div class="s-manager-group">
+			<label class="s-manager-label">Url</label>
+			<input type="text" name="" class="s-manager-input" placeholder="Website url">
+		</div>
+		<div class="s-manager-group">
+			<label class="s-manager-label">Description</label>
+			<textarea name="" class="s-manager-textarea" rows="4" placeholder="Website description"></textarea>
+		</div>
+		<div class="s-manager-group">
+			<label class="s-manager-label">Keys</label>
+			<textarea name="" class="s-manager-textarea" rows="4" placeholder="Website keys"></textarea>
+		</div>
+		<div class="s-manager-group s-manager-align">
+			<button class="s-manager-button">Save</button>
+		</div>
+	</div>
+</section>
 
 <?php include("footer.php"); ?>
