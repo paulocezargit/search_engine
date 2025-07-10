7f57a2e5f2ba25e20ad7681c506439b06d79fce5
Paulo Cezar <32718867+paulocezarphp@users.noreply.github.com>
2025-07-09 22:22:52 -0300

    Making some small changes
    This time we made a small and subtle change to our code by adding some controls to the .htaccess file to improve page navigation.

--- manager.php
+++ manager.php
@@ -1,30 +1,31 @@
 <?php include("header.php"); ?>
 
 <section class="s-manager">
-	<form method="POST" action="assets/php/actions/register-manager.php" class="s-manager-box">
+	<form id="manager-form" method="POST" action="assets/php/actions/register-manager.php" class="s-manager-box">
 		<h1 class="s-manager-title">Register Site</h1>
-		
+
 		<div class="s-manager-group">
 			<label class="s-manager-label">Name</label>
-			<input type="text" name="name" class="s-manager-input" placeholder="Website name">
+			<input type="text" name="name" id="name" class="s-manager-input" placeholder="Website name">
 		</div>
 		<div class="s-manager-group">
 			<label class="s-manager-label">Url</label>
-			<input type="text" name="url" class="s-manager-input" placeholder="Website url">
+			<input type="text" name="url" id="url" class="s-manager-input" placeholder="Website url">
 		</div>
 		<div class="s-manager-group">
 			<label class="s-manager-label">Description</label>
-			<textarea name="description" class="s-manager-textarea" rows="4" placeholder="Website description"></textarea>
+			<textarea name="description" id="description" class="s-manager-textarea" rows="4" placeholder="Website description"></textarea>
 		</div>
 		<div class="s-manager-group">
 			<label class="s-manager-label">Keys</label>
-			<textarea name="keys" class="s-manager-textarea" rows="4" placeholder="Website keys"></textarea>
+			<textarea name="keys" id="keys" class="s-manager-textarea" rows="4" placeholder="Website keys"></textarea>
 		</div>
 		<input type="hidden" name="type_register" value="save">
 		<div class="s-manager-group s-manager-align">
-			<button class="s-manager-button">Save</button>
+			<button type="submit" class="s-manager-button" id="manager-submit">Save</button>
 		</div>
 	</form>
+
 </section>
 
 <?php include("footer.php"); ?>
